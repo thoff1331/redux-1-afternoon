cf4213005ed35af6dce3516bbcd40c499fae171d
Trevor hoffman <trevorhoffman@Trevors-MacBook-Air.local>
2019-04-15 15:03:28 -0500

    done with redux-1

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -1,27 +1,44 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import store, { ADD_INSTRUCTION, ADD_RECIPE } from "./../../store";
 
 class Instructions extends Component {
   constructor(props) {
     super(props);
+    const reduxState = store.getState();
     this.state = {
-      instructions: [],
+      instructions: reduxState.instructions,
       input: ""
     };
   }
+
+  componentDidMount() {
+    store.subscribe(() => {
+      const reduxState = store.getState();
+      this.setState({
+        instructions: reduxState.instructions
+      });
+    });
+  }
+
   handleChange(val) {
     this.setState({
       input: val
     });
   }
   addInstruction() {
-    // Send data to Redux state
+    store.dispatch({
+      type: ADD_INSTRUCTION,
+      payload: this.state.input
+    });
     this.setState({
       input: ""
     });
   }
   create() {
-    // Create new recipe in Redux state
+    store.dispatch({
+      type: ADD_RECIPE
+    });
   }
   render() {
     const instructions = this.state.instructions.map((instruction, i) => {
@@ -31,7 +48,7 @@ class Instructions extends Component {
       <div className="List forms">
         <h2>Instructions:</h2>
         <div className="form_items_container">
-          <ol className='list'>{instructions}</ol>
+          <ol className="list">{instructions}</ol>
         </div>
         <div className="add_container">
           <input
@@ -43,10 +60,12 @@ class Instructions extends Component {
           </button>
         </div>
         <Link to="/add/ingredients">
-          <button className='left_button'>Previous</button>
+          <button className="left_button">Previous</button>
         </Link>
         <Link to="/">
-          <button className='right_button' onClick={() => this.create()}>Create</button>
+          <button className="right_button" onClick={() => this.create()}>
+            Create
+          </button>
         </Link>
       </div>
     );
